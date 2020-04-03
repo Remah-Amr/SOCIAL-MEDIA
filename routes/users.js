589302d8b9a7fbbb21c1aa1b50fc1c017ea9dd58
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 02:51:50 +0200

    bulid a user register endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router()
+const {User, Validate} = require('../models/user');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
-router.post('/register', (req, res) => {
+
+router.post('/register', async (req, res) => {
+
+    const { error } = Validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({email : req.body.email});
+    if(user) return res.status(400).send(`User Already register`); 
+
+    user = new User(_.pick(req.user, ['name', 'email', 'password', 'isAdmin']));
     
-})
+    const salt  = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+});
 
 module.exports = router
