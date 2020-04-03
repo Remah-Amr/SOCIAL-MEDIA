593bd67f580b3dec2b088f40b0ad5324eea4c2dc
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 21:40:48 +0200

    create edit user endpoint

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,17 @@ const {User, validate} = require('../models/user');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const login = require('../middelware/login');
+const Joi = require('joi');
 
 router.get('/me', login, async (req, res) => {
     const user = await User.findById(req.user._id).select('-password');
     res.send(user);
-})
+});
+
+// router.get('/', async(req, res) => {
+//     const users = User.find().sort('name');
+//     res.send(users)
+// });
 
 router.post('/register', async (req, res) => {
     const { error } = validate(req.body);
@@ -29,4 +35,28 @@ router.post('/register', async (req, res) => {
     res.header('x-auth-token', token).send(_.pick(user, ['name', 'email']));
 });
 
-module.exports = router
+router.put('/', login, async (req, res) => {
+    const { error } =  validateUser(req.bod);
+    if(error) return res.status(400).send(error.details[0].message);
+    
+    const user = await User.findById(req.user._id);
+    
+    user.name = req.body.name;
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(req.body.password, salt);
+
+    await user.save();
+    res.send(_.pick(user, ['name', 'email']));
+});
+
+
+function validateUser(req) {
+    const schema = {
+        name : Joi.string().min(5).max(50).required(),
+        password: Joi.string().min(8).max(1024).required()
+    }
+
+    return Joi.validate(req, schema);
+}
+
+module.exports = router;
