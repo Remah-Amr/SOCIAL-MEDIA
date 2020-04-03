26bdef96270e6f336bad2536b6fe9ba51982d8ca
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 03:12:45 +0200

    complete connection to DB script

--- startup/db.js
+++ startup/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 module.exports = function() {
-    mongoose.connect();
+    const db = config.get('db');
+    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+        .then(() => console.log(`connected to ${db}...`));
 }
