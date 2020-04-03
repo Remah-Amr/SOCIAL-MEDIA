10ee537652326f9970f02a9ea1a7c512edf98319
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 02:52:43 +0200

    bulid a user model

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 1024
+    },
+    isAdmin: Boolean
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name : Joi.string().min(5).max(50).required(),
+        email: Joi.string().min(10).max(255).required().email(),
+        password: Joi.string().min(8).max(1024).required(),
+        
+    }
+    return Joi.validate(user, schema);
+}
+
+module.User = User;
+module.Validate = validateUser;
