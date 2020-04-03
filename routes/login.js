2ec4dded298c85e0b9473fedd35ef656e0a4d2ba
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 05:05:28 +0200

    login endpoint

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.router();
 const Joi = require('joi');
+const {User} = require('../models/user');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const {error} = validation(req.body);
+    if(error) return res.send(error.details[0].message);
+    
+    let user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Invalid email or password.');
 
-})
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send("Invalid email or password.");
 
+    const token = user.generateAuthToken();
+    res.json({ "token": token });
+
+});
 
 function validation(req){
     const schema = {
