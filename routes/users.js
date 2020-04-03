df61a4d2cbbd3577ea8a5034d2c71ce69ca69b5a
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 04:30:30 +0200

    fix error

--- routes/users.js
+++ routes/users.js
@@ -12,14 +12,15 @@ router.post('/register', async (req, res) => {
     let user = await User.findOne({email : req.body.email});
     if(user) return res.status(400).send(`User Already register`); 
 
-    user = new User(_.pick(req.user, ['name', 'email', 'password', 'isAdmin']));
+    user = new User(_.pick(req.body, ['name', 'email', 'password', 'isAdmin']));
+    
     
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
 
     await user.save();
 
-    res.status(200).send(_.pick(user, ['name']))
+   
 });
 
 module.exports = router
