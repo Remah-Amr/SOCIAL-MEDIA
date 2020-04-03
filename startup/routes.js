74b5a588c7fe129b0adcb2348a8ba45c67c37610
mohamedelraghy <elraghy8@gmail.com>
2020-04-03 05:23:55 +0200

    add login endpoint

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const users = require('../routes/users');
+const login = require('../routes/login');
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/api/users', users);
+    app.use('/api/login', login);
 }
