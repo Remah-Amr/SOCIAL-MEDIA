e00669377e28f7d4cc2ee31d3dccfe1c21dc0ab6
mohamedelraghy <elraghy8@gmail.com>
2020-04-04 04:28:21 +0200

    add post endpoint

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('../routes/users');
 const login = require('../routes/login');
+const post = require('../routes/post');
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/api/users', users);
     app.use('/api/login', login);
+    app.use('/api/post', post);
 }
