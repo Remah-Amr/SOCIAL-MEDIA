863ae104f3f912c5aaae6457f4505ac8fb5e73ae
mohamedelraghy <elraghy8@gmail.com>
2020-04-04 02:10:15 +0200

    make sure that we enter password for mail env var

--- startup/config.js
+++ startup/config.js
@@ -1,6 +1,6 @@
 const config = require('config');
 
 module.exports = function(){
-    if (!config.get('jwtKey')) 
-        throw new Error(`FATAL ERROR: jwtKey is not defined.`);
+    if (!config.get('jwtKey')) throw new Error(`FATAL ERROR: jwtKey is not defined.`);
+    if (!config.get('mailPassword')) throw new Error(`FATAL ERROR: mailPassword is not defined.`);
 }
